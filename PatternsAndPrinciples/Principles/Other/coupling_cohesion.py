408f7a945ac00e3e2bcb9e887eb4a97ae40a1f90
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2022-10-16 07:10:51 +0300

    Update dependency comments

--- PatternsAndPrinciples/Principles/Other/coupling_cohesion.py
+++ PatternsAndPrinciples/Principles/Other/coupling_cohesion.py
@@ -13,29 +13,29 @@ High Coupling
 Low Cohesion
  - Class has multiple functionalities; fetches data, alert decision logic and email sending
 """
-class ExampleStart():
+class ExampleStart:
     URL = 'https://dummy-sensors.azurewebsites.net/api/sensor'
     def execute_logic(self, id):
-        sensor_response = urllib.request.urlopen(f'{self.URL}/{id}')  # <-- dependency
+        sensor_response = urllib.request.urlopen(f'{self.URL}/{id}')  # <-- dependency to urllib
         payload = sensor_response.read()
         sensor =  json.loads(payload.decode('utf-8'))
 
         message  = 'alert' if sensor['data'] > 25 else 'ok'  # Actual business logic of the class
 
-        msg = EmailMessage()   # <-- dependency
+        msg = EmailMessage()   # <-- dependency to EmailMessage
         msg.set_content(message)
         msg['Subject'] = f'Message from sensor: {sensor["id"]}'
         msg['From'] = 'alert@me.com'
         msg['To'] = 'receiver@you.com'
 
         # ConnectionRefusedError: [Errno 111] Connection refused
-        # s = smtplib.SMTP('localhost')
+        # s = smtplib.SMTP('localhost')  # <-- dependency to smtplib
         # s.send_message(msg)
         # s.quit()
         print(f'Sent: {msg}')
 
 
-class SensorStore():
+class SensorStore:
     URL = 'https://dummy-sensors.azurewebsites.net/api/sensor'
     def get_sensor(self, id):
         sensor_response = urllib.request.urlopen(f'{self.URL}/{id}')
@@ -43,7 +43,7 @@ class SensorStore():
         return json.loads(payload.decode('utf-8'))
 
 
-class EmailSender():
+class EmailSender:
     def send_message(self, sensor_id, message):
         msg = EmailMessage()
         msg.set_content(message)
@@ -66,18 +66,18 @@ High Cohesion
  - Class does a single thing; decide the message type we should send
  - Data fetching and Email sending are in own classes
 """
-class ExampleMiddle():
+class ExampleMiddle:
     def execute_logic(self, id):
-        store = SensorStore()   # <-- dependency
+        store = SensorStore()   # <-- dependency to SensorStore
         sensor = store.get_sensor(id)
 
         message  = 'alert' if sensor['data'] > 25 else 'ok'  # Actual business logic of the class
 
-        message_sender = EmailSender()   # <-- dependency
+        message_sender = EmailSender()   # <-- dependency to EmailSender
         message_sender.send_message(sensor['id'], message)
 
 
-class SensorStoreDB():
+class SensorStoreDB:
     def get_sensor(self, id):
         return self.connection.execute('SELECT * FROM sensorts WHERE id = %s LIMIT 1', [id])
 
@@ -90,7 +90,7 @@ High Cohesion
  - Class does a single thing, decide the message type we should send
  - Data fetching and Email sending are in own classes
 """
-class ExampleEnd():
+class ExampleEnd:
     def __init__(self, store, message_sender):
         self.store = store
         self.message_sender = message_sender
